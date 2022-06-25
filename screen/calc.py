73c0dfec89c4343d82a2482d719138dc0609bc84
Merc4tor <63878555+Merc4tor@users.noreply.github.com>
2022-06-26 00:20:45 +0200

    Update calc.py
    fixes some conversion issues

--- screen/calc.py
+++ screen/calc.py
@@ -1,25 +1,26 @@
 from settings.settings import *
 
 def calc_game_to_pixel_coords(x=None, y=None):
-    if x != None and y != None:
-        return ( x / simfieldsizex * (simFieldX2 - simFieldX1) + simFieldX1, (simfieldsizey - y) / simfieldsizey * (simFieldY2 - simFieldY1))
+    if isinstance(x, tuple):
+        if isinstance(x[0], (int, float)) and isinstance(x[1], (int, float)):
+            return ( x[0] / simfieldsizex * (simFieldX2 - simFieldX1) + simFieldX1, (simfieldsizey- x[1]) / simfieldsizey * (simFieldY2 - simFieldY1) - simFieldY1)
     elif x != None:
         return  x / simfieldsizex * (simFieldX2 - simFieldX1) + simFieldX1
     elif y != None:
-        return  (simfieldsizey - y) / simfieldsizey * (simFieldY2 - simFieldY1) + simFieldY1
-    else:
-        print("did not put in a x or y in calc_game_to_pixel_coords()")
-
+        return  (simfieldsizey - y) / simfieldsizey * (simFieldY2 - simFieldY1) - simFieldY1
+    else:    
+        print("did not put in a valid x, y or a coord in calc_game_to_pixel_coords()")
+        
 def calc_pixel_to_game_coords(x=None, y=None):
-    if x != None and y != None:
-        return ( (x - 10) / (simFieldX2 - simFieldX1) * simfieldsizex, (simFieldY2 - y) / (simFieldY2 - simFieldY1) * simfieldsizey )
+    if isinstance(x, tuple):
+        if isinstance(x[0], (int, float)) and isinstance(x[1], (int, float)):
+            return ((x[0] - simFieldX1) / (simFieldX2 - simFieldX1) * simfieldsizex, (simFieldY2 - x[1] - simFieldY1) / (simFieldY2 - simFieldY1) * simfieldsizey)
     elif x != None:
-        return   (x - 10) / (simFieldX2 - simFieldX1) * simfieldsizex
-
+        return  (x - 10) / (simFieldX2 - simFieldX1) * simfieldsizex
     elif y != None:
-        return  (simFieldY2 - y) / (simFieldY2 - simFieldY1) * simfieldsizey
-    else:
-        print("did not put in a x or y in calc_game_to_pixel_coords()")
+        return  (simFieldY2 - y - simFieldY1) / (simFieldY2 - simFieldY1) * simfieldsizey
+    else:    
+        print("did not put in a valid x, y or a coord in calc_game_to_pixel_coords()")
 
 #calculate text coords: [[xtop, ytop],[xend, yend]]
 def calc_text_coords(text="test",centre=(100, 100), font=fontfreesan):
